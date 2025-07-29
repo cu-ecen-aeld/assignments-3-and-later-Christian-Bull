922b32a417a8989e5af3f108f7b0fd113dd5822c
Christian Bull <bullcs95@gmail.com>
2025-07-29 11:37:44 -0600

    completed assignment

---
+++ finder-app/finder.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+set -u
+
+if [ $# -lt 2 ]; then
+    echo "Parameters not specified"
+    exit 1
+fi
+
+dir=$1
+
+
+if [[ ! -z "$dir" ]]; then
+    echo "$1 exists"
+    
+    grep -crnw $dir -e $2
+
+    numfiles=$(eval grep -crnw $dir -e $2 | wc -l)
+    nummatches=$(eval grep -rnw $dir -e $2 | wc -l)
+
+    echo "The number of files are $numfiles and the number of matching lines are $nummatches"
+else
+	echo "$1 not found"
+	exit 1
+fi
---
+++ finder-app/writer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+set -u
+
+if [ $# -lt 2 ]; then
+    echo "Parameters not specified"
+    exit 1
+fi
+
+file=$1
+
+mkdir -p "${file%/*}" && touch "$file"
+
+echo $2 > $1

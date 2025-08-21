5c2051efb9724005aa89433632ca4df9aaf73a5a
Christian Bull <cbull.projects@gmail.com>
2025-08-20 20:21:32 -0600

    add manual linux script

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -7,11 +7,19 @@ set -u
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_VERSION=v5.15.163
+
+# older kernel version requires too many downgrades, not worth using
+# KERNEL_VERSION=v5.15.163
+# CONFIG_PREFIX=/tmp/aeld/rootfs ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-
+
+
+KERNEL_VERSION=v6.1.148
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+CROSS_COMPILE_DIR=/opt/arm-gnu-toolchain/arm-gnu-toolchain-14.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu
+WRITER_APP=/home/cbull/projects/cu-boulder/ecea-5305/assignment-1-Christian-Bull/finder-app
 
 if [ $# -lt 1 ]
 then
@@ -35,19 +43,32 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+
+    # generate default config
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+    # builds kernal
+    make -j 8 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
+
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    rm  -rf ${OUTDIR}/rootfs
 fi
 
 # TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs && cd ${OUTDIR}/rootfs 
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -61,20 +82,43 @@ else
 fi
 
 # TODO: Make and install busybox
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=/${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+cd ${OUTDIR}/rootfs
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter" | awk -F': ' '{print $2}' | tr -d '[]'
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+# should I automate this? seemed rather annoying and specific to track down
+cp ${CROSS_COMPILE_DIR}/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${CROSS_COMPILE_DIR}/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${CROSS_COMPILE_DIR}/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp ${CROSS_COMPILE_DIR}/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ${WRITER_APP}
+make clean
+make writer CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+cp writer start-qemu-app.sh start-qemu-terminal.sh autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp finder.sh finder-test.sh ${OUTDIR}/rootfs/home/
+
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp conf/username.txt conf/assignment.txt ${OUTDIR}/rootfs/home/conf
 
 # TODO: Chown the root directory
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+
+gzip -f ${OUTDIR}/initramfs.cpio
